320d404e5c798bc37cb50a11c2dce63c77fb45f1
Trung Le <leductrungxf@gmail.com>
2022-01-18 17:50:49 +0100

    Refactor `completer` and `completer-extension` package

--- packages/tooltip/src/tokens.ts
+++ packages/tooltip/src/tokens.ts
@@ -27,7 +27,7 @@ export interface ITooltipManager {
 }
 
 /**
- * A namespace for `ICompletionManager` interface specifications.
+ * A namespace for `ITooltipManager` interface specifications.
  */
 export namespace ITooltipManager {
   /**
