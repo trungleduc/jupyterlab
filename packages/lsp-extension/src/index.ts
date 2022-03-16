6434893e6062f9c73620c00187592980318e1148
Duc Trung LE <leductrungxf@gmail.com>
2022-03-16 15:17:17 +0100

    Create lsp-extension

---
+++ packages/lsp-extension/src/index.ts
@@ -0,0 +1,43 @@
+/* -----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+/**
+ * @packageDocumentation
+ * @module lsp-extension
+ */
+
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { ITranslator } from '@jupyterlab/translation';
+
+/**
+ * The default terminal extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  activate,
+  id: '@jupyterlab/terminal-extension:plugin',
+  requires: [ISettingRegistry, ITranslator],
+  autoStart: true
+};
+
+/**
+ * Activate the lsp plugin.
+ */
+function activate(
+  app: JupyterFrontEnd,
+  settingRegistry: ISettingRegistry,
+  translator: ITranslator
+): void {
+  console.log('open called');
+}
+
+/**
+ * Export the plugin as default.
+ */
+export default plugin;
