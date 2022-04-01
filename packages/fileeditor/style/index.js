839cb26f4a28e03a37cc3a88ae09efabb07ebb82
Duc Trung LE <leductrungxf@gmail.com>
2022-04-01 17:41:05 +0200

    Update version

--- packages/fileeditor/style/index.js
+++ packages/fileeditor/style/index.js
@@ -8,6 +8,7 @@ import '@lumino/widgets/style/index.js';
 import '@jupyterlab/ui-components/style/index.js';
 import '@jupyterlab/apputils/style/index.js';
 import '@jupyterlab/codeeditor/style/index.js';
+import '@jupyterlab/codemirror/style/index.js';
 import '@jupyterlab/docregistry/style/index.js';
 
 import './base.css';
